fbc7d3ea364b6a0e4252504cc85e1ff9235360c3
Jan Smolka <onkelsmo@gmail.com>
2014-06-08 14:51:12 +0200

    ADD: service to list contacts

---
+++ src/php/Services/Contacts.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * @copyright jsmolka 
+ * @link https://github.com/onkelsmo/contactManaging
+ */
+namespace Services;
+
+class Contacts {
+	private static function db() {
+		$db = new \mysqli('localhost', 'buch', 'buch', 'buch');
+		$db->set_charset("utf8");
+		return $db;
+	}
+
+	private function getUserId() {
+		return 1;
+	}
+
+	public function listContacts($contactId = -1) {
+		$db = self::db();
+		try {
+			$stmt = $db->prepare("
+                    SELECT 
+                        k.knt_id, 
+                        t.ett_id, 
+                        t.ett_name, 
+                        t.ett_eindeutig, 
+                        e.etg_id, 
+                        e.etg_wert 
+                    FROM
+                        kontakt k
+                    INNER JOIN
+                        eintrag e
+                    ON
+                        k.knt_id = e.knt_id
+                    INNER JOIN
+                        eintragstyp t
+                    ON 
+                        t.ett_id = e.ett_id
+                    WHERE
+                        k.bnz_id = ?
+                    AND
+                        (k.knt_id = ? or ? = -1)
+                    ORDER BY
+                        k.knt_id, 
+                        t.ett_reihenfolge,
+                        t.ett_name,
+                        e.etg_id
+                    ");
+			if (!$stmt) {
+				throw new SQLException($db->error, $db->errno);
+			}
+
+			$userId = $this->getUserId();
+
+			$stmt->bind_param("iii", $userId, $contactId, $contactId);
+			$stmt->bind_result(
+					$contactId, $typeId, $typeName, $unique, $valueId, $value);
+			$stmt->execute();
+
+			$data = array();
+			$lastContact = -1;
+			$lastTypeId = null;
+
+			while ($stmt->fetch()) {
+				if ($contactId != $lastContact) {
+					$lastContact = $contactId;
+					$insert = -1;
+				}
+				if ($lastTypeId != $typeId) {
+					++$insert;
+					$data[$contactId][$insert]->name = $lastTypeName = $typeName;
+					$data[$contactId][$insert]->id = $lastTypeId = $typeId;
+				}
+				if ($unique) {
+					$data[$contactId][$insert]->insert = Array(
+						'id' => $valueId,
+						'wert' => $value);
+				} else {
+					$data[$contactId][$insert]->insert[] = Array(
+						'id' => $valueId,
+						'wert' => $value);
+				}
+			}
+
+			$stmt->close();
+			$db->close();
+
+			$result = Array("data" => $data);
+			return $result;
+		} catch (\Exception $e) {
+			if ($stmt) {
+				$stmt->close();
+			}
+			if ($db) {
+				$db->close();
+			}
+			throw $e;
+		}
+	}
+
+}
